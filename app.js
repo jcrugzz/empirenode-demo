8f33ecbbadb288b60432abd56b2cc572e9248928
Jarrett Cruger <jcrugzz@gmail.com>
2014-10-21 18:33:15 -0400

    [new] add simple app

---
+++ app.js
@@ -0,0 +1,48 @@
+var http = require('http');
+var minimist = require('minimist');
+var g = require('godot');
+
+var argv = minimist(process.argv.slice(2));
+
+var port = argv.p || argv.port || 8080;
+var gPort = argv.g || argv.godot || 6000;
+
+var started = false;
+
+var godot = g.createClient({ type: 'tcp' });
+godot.connect(gPort);
+godot.on('connect', function () {
+  if (started) return;
+  started = true;
+
+  godot.produce({
+    service: 'app/init',
+    state: 'ok',
+    metric: 1,
+    description: 'Event sent to indicate a server just went back up',
+    meta: {
+      server: 'http://127.0.0.1:' + port
+    }
+  })
+});
+
+http.createServer(function(req, res) {
+  if (req.url === '/') {
+    res.writeHead(200);
+    res.setHeader('content-type', 'application/json');
+    res.end(JSON.stringify({ status: 'ok' }));
+    return;
+  }
+}).listen(3000);
+
+var up = setInterval(function () {
+  godot.produce({
+    service: 'app/uptime',
+    description: 'Uptime of server',
+    state: 'ok',
+    metric: process.uptime(),
+    meta: {
+      server: 'http://127.0.0.1:' + port
+    }
+  })
+}, 5000);
